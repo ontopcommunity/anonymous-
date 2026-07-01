4480123260cf26793e3a294a16bd3ec413411691
Nhật Minh(OnTop) <minh21092k9vn@gmail.com>
2026-07-01 20:49:45 +0700

    Update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
-// Khởi tạo file mới
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Anonymous Chat",
+  description: "Realtime anonymous chat"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  );
+}
